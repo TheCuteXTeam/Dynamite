7bed0547ed37281eb80e37c5f9e09092eba0bd8a
TheCuteXTeam <herkoumail@gmail.com>
2024-09-12 10:23:30 +0530

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/TheCuteXTeam/Princy",
+    "https://github.com/TheCuteXTeam/Dynamite",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
